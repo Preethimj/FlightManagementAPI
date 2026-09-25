fd12772cd11e28c86cf06d08e4a92f419ca32f92
Preethimj <39443397+Preethimj@users.noreply.github.com>
2026-09-25 12:48:16 +0530

    Add files via upload
    created repo

---
+++ FlightManagementAPI/FlightManagementAPI/src/test/java/com/example/FlightManagementAPI/FlightManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlightManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
