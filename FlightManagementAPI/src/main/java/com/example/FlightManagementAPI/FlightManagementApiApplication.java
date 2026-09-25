bfe0c36fbf42acbe95208658bb224d7d381e106b
Preethimj <39443397+Preethimj@users.noreply.github.com>
2026-09-25 12:50:55 +0530

    Add files via upload

---
+++ FlightManagementAPI/src/main/java/com/example/FlightManagementAPI/FlightManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagementApiApplication.class, args);
+	}
+
+}
